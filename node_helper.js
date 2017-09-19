fa28c31d31b566880a33b08240e368a07e5a0200
sharmmoh1983 <sharmmoh1983@gmail.com>
2017-09-19 16:17:35 +0000

    first commit

---
+++ node_helper.js
@@ -0,0 +1,38 @@
+/* Magic Mirror
+ * Module: MMM-Events
+ *
+ * By Mykle1
+ *
+ */
+const NodeHelper = require('node_helper');
+const request = require('request');
+
+
+
+module.exports = NodeHelper.create({
+
+    start: function() {
+        console.log("Starting node_helper for: " + this.name);
+    },
+
+    getEvents: function(url) {
+        request({
+            url: url,
+            method: 'GET'
+        }, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+
+                var result = JSON.parse(body).events;
+console.log("In get events node helper response", response.statusCode );
+                this.sendSocketNotification('EVENTS_RESULT', result);
+            }
+        });
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+console.log("In get events node helper");
+        if (notification === 'GET_EVENTS') {
+            this.getEvents(payload);
+        }
+    }
+});
